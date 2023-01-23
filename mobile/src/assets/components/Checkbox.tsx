b5aa24bb40b2005eb0c989895b136949032bf2f4
Giovanna Trigueiro <giovanna.trigueiro@gmail.com>
2023-01-23 10:06:13 -0300

    feat - New mobile features and styles

--- mobile/src/assets/components/Checkbox.tsx
+++ mobile/src/assets/components/Checkbox.tsx
@@ -1,28 +1,38 @@
 import colors from "tailwindcss/colors";
 import { Feather } from "@expo/vector-icons";
-import { TouchableOpacity, TouchableOpacityProps, View, Text } from "react-native";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+  Text,
+} from "react-native";
 
-interface Props extends TouchableOpacityProps{
+interface Props extends TouchableOpacityProps {
   title: string;
   checked?: boolean;
 }
 
-export function CheckBox({ title, checked = false, ...rest }: Props) {
+export function Checkbox({ title, checked = false, ...rest }: Props) {
   return (
     <TouchableOpacity
-      activeOpacity={0.5}
+      activeOpacity={0.7}
       className="flex-row mb-2 items-center"
       {...rest}
     >
       {checked ? (
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
-          <Feather name="check" size={20} color={colors.white}></Feather>
-        </View>
+        <Animated.View
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
+          <Feather name="check" size={20} color={colors.white} />
+        </Animated.View>
       ) : (
-        <View className="h-8 w-8 bg-zinc-900 rounded-lg items-center justify-center" />
+        <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       )}
 
-      <Text className="text-white text-base ml-3">{title}</Text>
+      <Text className="text-white text-base ml-3 font-semibold">{title}</Text>
     </TouchableOpacity>
   );
 }
