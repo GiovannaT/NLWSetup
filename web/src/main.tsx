f463d8791327064075754c2eb2f049f2976631a5
Giovanna Trigueiro <giovanna.trigueiro@gmail.com>
2023-01-25 20:19:02 -0300

    tailwind light theme pre-fix

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,10 @@
-import { App } from './App'
-import React from 'react'
-import './styles/globals.css'
-import ReactDOM from 'react-dom/client'
+import { App } from "./App";
+import React from "react";
+import "./styles/globals.css";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
